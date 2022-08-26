ca1eeb49824875fbbda7398cacd904a3af97fefc
wjdtj9656 <wjdtj9656@naver.com>
2022-08-27 01:13:50 +0900

    FEAT:can show item 10

--- src/components/Goods/Goods.tsx
+++ src/components/Goods/Goods.tsx
@@ -3,29 +3,54 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import { useEffect, useState } from "react";
 import itemDataApi from "../../api/ItemDataAPI";
+import { useInView } from "react-intersection-observer";
+interface Item {
+  id: number;
+  image: string;
+  prevPrice: string;
+  nextPrice: string;
+  title: string;
+  tag: string;
+  discount: string;
+  heartNum: number;
+  chatNum: number;
+  stock: number;
+}
 const Goods = () => {
-  const [nowItmes, setNowItems] = useState([]);
+  const [Items, setItems] = useState<Item[]>([]);
+  const [nowItems, setNowItems] = useState<Item[]>([]);
+  const [itemIndex, setItemIndex] = useState(0);
+
+  const [ref, inView] = useInView();
+  const showMoreItem = () => {
+    for (let i = itemIndex; i < itemIndex + 10; i++) {
+      nowItems.push(Items[i]);
+    }
+    setItemIndex(itemIndex + 10);
+    setNowItems([...nowItems]);
+  };
   useEffect(() => {
-    itemDataApi.get("dummy/itemData.json").then((item) => setNowItems(item.data.itemList));
+    itemDataApi.get("dummy/itemData.json").then((item) => {
+      setItems(item.data.itemList);
+    });
   }, []);
-  interface Item {
-    id: number;
-    image: string;
-    prevPrice: string;
-    nextPrice: string;
-    title: string;
-    tag: string;
-    discount: string;
-    heartNum: number;
-    chatNum: number;
-    stock: number;
-  }
+  useEffect(() => {
+    if (Items.length > 0) showMoreItem();
+  }, [Items]);
+  useEffect(() => {
+    if (nowItems.length > 0) showMoreItem();
+  }, [inView]);
   return (
     <>
       <div className={styles.goodsBox}>
-        {nowItmes.map((item: Item) => (
+        {nowItems.map((item: Item, index) => (
           <div className={styles.goods}>
-            <img src={item.image} className={styles.goodsImage} alt="itemImage" />
+            {nowItems.length - 1 === index && (
+              <img src={item.image} className={styles.goodsImage} alt="itemImage" ref={ref} />
+            )}
+            {nowItems.length - 1 !== index && (
+              <img src={item.image} className={styles.goodsImage} alt="itemImage" />
+            )}
             <div className={styles.explanation}>
               <div className={styles.goodsPricePrev}>{item.prevPrice}</div>
               <div className={styles.goodsPriceNext}>{item.nextPrice}</div>
@@ -42,6 +67,7 @@ const Goods = () => {
             </div>
           </div>
         ))}
+        <button onClick={showMoreItem}>zzzzzzzzzz</button>
       </div>
     </>
   );
