6330cfa302a270639cd8a062a4a1cd352576d184
wjdtj9656 <wjdtj9656@naver.com>
2022-08-26 21:53:06 +0900

    FEAT: Can show data

--- src/components/Goods/Goods.tsx
+++ src/components/Goods/Goods.tsx
@@ -8,12 +8,24 @@ const Goods = () => {
   useEffect(() => {
     itemDataApi.get("dummy/itemData.json").then((item) => setNowItems(item.data.itemList));
   }, []);
+  interface Item {
+    id: number;
+    image: string;
+    prevPrice: string;
+    nextPrice: string;
+    title: string;
+    tag: string;
+    discount: string;
+    heartNum: number;
+    chatNum: number;
+    stock: number;
+  }
   return (
     <>
       <div className={styles.goodsBox}>
-        {nowItmes.map((item) => (
+        {nowItmes.map((item: Item) => (
           <div className={styles.goods}>
-            <img src={item.image} className={styles.goodsImage} />
+            <img src={item.image} className={styles.goodsImage} alt="itemImage" />
             <div className={styles.explanation}>
               <div className={styles.goodsPricePrev}>{item.prevPrice}</div>
               <div className={styles.goodsPriceNext}>{item.nextPrice}</div>
@@ -30,21 +42,6 @@ const Goods = () => {
             </div>
           </div>
         ))}
-        {/* <div className={styles.goods}>
-          <img src="/assets/images/grape.PNG" className={styles.goodsImage} />
-          <div>
-            <div className={styles.goodsPricePrev}>17000</div>
-            <div className={styles.goodsPriceNext}>17000</div>
-            <h3 className={styles.goodsTitle}>대부도/송산 컴벨포도</h3>
-            <button className={styles.goodsEvent}>신세계 포인트 적립시 3천원 할인</button>
-            <div className={styles.sideBtnGroup}>
-              <FavoriteIcon className={styles.heartBtn}>heart</FavoriteIcon>
-              <h6 className={styles.btnText}>50</h6>
-              <ChatBubbleOutlineIcon className={styles.chatBtn}>chat</ChatBubbleOutlineIcon>
-              <h6 className={styles.btnText}>50</h6>
-            </div>
-          </div>
-        </div> */}
       </div>
     </>
   );
